cf72b8656157a7fcba39f669fcfc078557c427a0
Auqherus <patrykromanowski@gmail.com>
2024-05-02 20:51:09 +0200

    Simple Exercise for python

---
+++ Main.py
@@ -0,0 +1,7 @@
+
+thisArticle = '''Monty Python (also collectively known as the Pythons)[2][3] were a British comedy troupe formed in 1969 consisting of Graham Chapman, John Cleese, Terry Gilliam, Eric Idle, Terry Jones, and Michael Palin. 
+The group came to prominence for the sketch comedy series Monty Python's Flying Circus, which aired on the BBC from 1969 to 1974. 
+Their work then developed into a larger collection that included live shows, films, albums, books, and musicals; 
+their influence on comedy has been compared to the Beatles' influence on music.[4][5][6] Their sketch show has been called "an important moment in the evolution of television comedy . My own text here xD".[7]'''
+
+print(thisArticle.lower())
